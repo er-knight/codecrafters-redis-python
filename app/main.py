c2ddbc15c0d75d99bd7ce291c10aba2f4b16faab
er-knight <60701544+er-knight@users.noreply.github.com>
2024-04-22 23:18:59 +0530

    stage 3: respond to multiple ping

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 import asyncio
 
 async def handler(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-    command = await reader.readline()
-    print(command)
-    writer.write('+PONG\r\n'.encode())
-    await writer.drain() 
+    while True:
+        command = await reader.readline()
+        print(command)
+        writer.write('+PONG\r\n'.encode())
+        await writer.drain() 
 
 async def main():
     server = await asyncio.start_server(handler, host='127.0.0.1', port=6379)
