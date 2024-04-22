ade507fb149d349e106b59484fd138834faa6ac0
er-knight <60701544+er-knight@users.noreply.github.com>
2024-04-22 22:57:29 +0530

    stage 2: respond to ping

--- app/main.py
+++ app/main.py
@@ -1,14 +1,11 @@
 import asyncio
 
 async def handler(reader, writer):
-    data = await reader.read()
-    print(data)
     writer.write('+PONG\r\n'.encode())
     await writer.drain() 
 
 async def main():
     server = await asyncio.start_server(handler, host='127.0.0.1', port=6379)
-    print(server.sockets)
     await server.serve_forever()
 
 if __name__ == "__main__":
