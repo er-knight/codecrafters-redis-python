fa0a14b5113662301a8cc12919e3dda499398090
er-knight <60701544+er-knight@users.noreply.github.com>
2024-04-22 22:37:24 +0530

    stage 1: bind to a port

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 import asyncio
 
+async def handler(reader, writer):
+    _ = await reader.read()
+
 async def main():
-    server = await asyncio.start_server(lambda _: print('Hello World'), '127.0.0.1', 6379)
+    server = await asyncio.start_server(handler, '127.0.0.1', 6379)
 
 if __name__ == "__main__":
     asyncio.run(main())
